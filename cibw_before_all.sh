06c858193cb5e23131df8bad0d7776cccdc7082a
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-01-31 16:25:30 -0700

    clean up cibw_before_all.sh

--- cibw_before_all.sh
+++ cibw_before_all.sh
@@ -2,14 +2,14 @@
 
 set -e -x
 
-echo "STEP Cleaning old files..."
-./clean.sh
+# echo "STEP Cleaning old files..."
+# yes | rm -fr _libhfst*.so build/ dist/ wheelhouse/
 
 echo "STEP Configuring..."
 cd hfst_src/
 autoreconf -i  # Is this necessary?
 ./configure --enable-all-tools --with-readline --enable-fsmbook-tests
-make clean
+# make clean
 make
 
 echo "STEP Making flex/yacc files..."
