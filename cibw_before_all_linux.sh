e2fa7384d822d8c44268941c20b387196af563c0
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-02-08 11:55:56 -0700

    apt -> yum

--- cibw_before_all_linux.sh
+++ cibw_before_all_linux.sh
@@ -2,9 +2,21 @@
 
 set -e -x
 
-apt-get -qy update
-apt-get -qfy install --no-install-recommends build-essential automake  \
-	autotools-dev pkg-config python3-dev python3-setuptools swig bison  \
-	flex libicu-dev libreadline-dev libtool zlib1g-dev
+# apt-get -qfy install --no-install-recommends build-essential automake  \
+# 	autotools-dev pkg-config python3-dev python3-setuptools swig bison  \
+# 	flex libicu-dev libreadline-dev libtool zlib1g-dev
+
+# manually install swig 4 since yum has 3 :(
+yum update -d1 && yum install -y bison flex
+yum install -y pcre pcre-devel  # needed to compile swig
+curl https://managedway.dl.sourceforge.net/project/swig/swig/swig-4.0.2/swig-4.0.2.tar.gz \
+        -o /tmp/swig-4.0.2.tar.gz
+pushd /tmp
+tar -zxvf swig-4.0.2.tar.gz
+cd swig-4.0.2
+./configure
+make
+make install
+popd
 
 ./build_hfst.sh
