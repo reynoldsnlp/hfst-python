691f5257f8c12e59a1454d701ba9ef0f99be3a2a
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-02-08 11:03:42 -0700

    remove brew update

--- cibw_before_all_macos.sh
+++ cibw_before_all_macos.sh
@@ -2,7 +2,6 @@
 
 set -e -x
 
-brew update
 brew install build-essential automake autotools-dev pkg-config python3-dev  \
         python3-setuptools swig bison flex libicu-dev libreadline-dev  \
 	libtool zlib1g-dev
