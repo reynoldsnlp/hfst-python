84e45247b05768151753f4d51f49748510c193e4
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-02-08 12:27:49 -0700

    yum install libicu-devel

--- cibw_before_all_linux.sh
+++ cibw_before_all_linux.sh
@@ -7,12 +7,12 @@ set -e -x
 # 	flex libicu-dev libreadline-dev libtool zlib1g-dev
 
 # manually install swig 4 since yum has 3 :(
-yum update -d1 && yum install -y bison flex
+yum update -d1 && yum install -y bison flex libicu-devel
 yum install -y pcre pcre-devel  # needed to compile swig
 curl https://managedway.dl.sourceforge.net/project/swig/swig/swig-4.0.2/swig-4.0.2.tar.gz \
         -o /tmp/swig-4.0.2.tar.gz
 pushd /tmp
-tar -zxvf swig-4.0.2.tar.gz
+tar -zxf swig-4.0.2.tar.gz
 cd swig-4.0.2
 ./configure
 make
