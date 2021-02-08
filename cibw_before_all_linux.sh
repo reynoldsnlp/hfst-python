6e5063bb05baa6bdfe26fcc4e062b200fbf220a6
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-02-08 11:45:19 -0700

    +x

--- cibw_before_all_linux.sh
+++ cibw_before_all_linux.sh
@@ -2,8 +2,8 @@
 
 set -e -x
 
-sudo apt-get -qy update
-sudo apt-get -qfy install --no-install-recommends build-essential automake  \
+apt-get -qy update
+apt-get -qfy install --no-install-recommends build-essential automake  \
 	autotools-dev pkg-config python3-dev python3-setuptools swig bison  \
 	flex libicu-dev libreadline-dev libtool zlib1g-dev
 
