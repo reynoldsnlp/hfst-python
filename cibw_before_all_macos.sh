ebd1bc38447e7361e55c92c3b3019ec26c6d8fac
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-02-10 09:10:51 -0700

    macos links to bison/flex

--- cibw_before_all_macos.sh
+++ cibw_before_all_macos.sh
@@ -3,5 +3,8 @@
 set -e -x
 
 brew install automake bison flex swig
+export PATH="/usr/local/opt/bison/bin:/usr/local/opt/flex/bin:$PATH"
+export CPPFLAGS="-I/usr/local/opt/flex/include"
+export LDFLAGS="-L/usr/local/opt/bison/lib -L/usr/local/opt/flex/lib"
 
 source ./build_hfst.sh
