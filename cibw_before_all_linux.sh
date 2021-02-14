d9592f7ff040a6675ff311aa9f095343bc781990
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-02-13 20:21:57 -0700

    fix tests and on manylinux2010 upgrade bison

--- cibw_before_all_linux.sh
+++ cibw_before_all_linux.sh
@@ -15,5 +15,17 @@ make
 make install
 popd
 
+
+BISON_VERSION=$(bison --version | python -c "import re, sys; print(re.search(r'([0-9]+)\.[0-9]+\.[0-9]+.*', sys.stdin.read(), re.S).group(1))")
+echo ${BISON_VERSION}
+if [[ ${BISON_VERSION} -le 7 ]]; then
+    echo "Bison is too old. Upgrading...";
+    curl http://springdale.princeton.edu/data/springdale/6/x86_64/os/Computational/bison3-3.0.2-3.sdl6.x86_64.rpm > bison3-3.0.2-3.sdl6.x86_64.rpm
+    rpm -Uvh springdale-computational*rpm
+    yum install -y bison3
+else
+    echo "Bison is new enough. No upgrade needed.";
+fi
+
 ./build_hfst.sh
 python setup.py build_ext
