bbb4d67fd8c97208674db69c84861c3e98601172
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-02-26 20:49:10 -0700

    brew install glib might be targeted for 10.9?

--- scripts/cibw_before_all_macos.sh
+++ scripts/cibw_before_all_macos.sh
@@ -5,7 +5,7 @@
 
 set -e -x
 
-brew install automake bison flex  # to build from source
+brew install automake bison flex glib  # to build hfst from source
 export PATH="/usr/local/opt/bison/bin:/usr/local/opt/flex/bin:$PATH"
 export CPPFLAGS="-I/usr/local/opt/flex/include"
 export LDFLAGS="-L/usr/local/opt/bison/lib -L/usr/local/opt/flex/lib"
@@ -25,5 +25,17 @@ brew install swig
 export MACOSX_DEPLOYMENT_TARGET=10.9
 
 # Build HFST from source
-./scripts/build_hfst.sh
+echo "deployment target: ${MACOSX_DEPLOYMENT_TARGET}"
+cd hfst_src/
+autoreconf -fvi
+./configure --disable-static  \
+	    --enable-all-tools  \
+	    --with-readline  \
+	    --with-unicode-handler=glib  \  # TODO glib
+	    MACOSX_DEPLOYMENT_TARGET=10.9
+make
+make check V=1 VERBOSE=1
+make install
+cd ..
+
 python setup.py build_ext
