141c7e5a79ae43e75a0481c84281ee7309a25885
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-03-03 21:40:49 -0700

    macos use icu

--- scripts/cibw_before_all_macos.sh
+++ scripts/cibw_before_all_macos.sh
@@ -6,9 +6,10 @@
 set -e -x
 
 brew install automake bison flex glib  # to build hfst from source
-export PATH="/usr/local/opt/bison/bin:/usr/local/opt/flex/bin:$PATH"
-export CPPFLAGS="-I/usr/local/opt/flex/include"
-export LDFLAGS="-L/usr/local/opt/bison/lib -L/usr/local/opt/flex/lib"
+export PATH="/usr/local/opt/icu4c/bin:/usr/local/opt/icu4c/sbin:/usr/local/opt/bison/bin:/usr/local/opt/flex/bin:$PATH"
+export CPPFLAGS="-I/usr/local/opt/flex/include -I/usr/local/opt/icu4c/include"
+export LDFLAGS="-L/usr/local/opt/bison/lib -L/usr/local/opt/flex/lib -L/usr/local/opt/icu4c/lib"
+export PKG_CONFIG_PATH="/usr/local/opt/icu4c/lib/pkgconfig"
 
 brew install swig
 
@@ -31,7 +32,7 @@ autoreconf -fvi
 ./configure --disable-static  \
 	    --enable-all-tools  \
 	    --with-readline  \
-	    --with-unicode-handler=glib  \
+	    --with-unicode-handler=icu  \
 	    MACOSX_DEPLOYMENT_TARGET=10.9
 make
 make check V=1 VERBOSE=1
--- scripts/cibw_before_all_windows.sh
+++ scripts/cibw_before_all_windows.sh
@@ -11,10 +11,13 @@ choco upgrade -y --no-progress winflexbison3 swig  # libicu-devel readline-devel
 #           base-devel  \
 #           bison  \
 #           flex  \
+#           mingw-w64-i686-toolchain  \
 #           mingw-w64-x86_64-toolchain  \
 #           # mingw-w64-cross-toolchain  \
 #           mingw-w64-x86_64-libtool  \
 #           mingw-w64-x86_64-icu  \
+#           icu-devel  \
+#           glib2-devel  \
 #           mingw-w64-x86_64-readline  \
 #           swig
 
