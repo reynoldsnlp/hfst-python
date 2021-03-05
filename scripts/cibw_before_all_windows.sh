1304be6932a5451565b9e2f9e87ccddcec235766
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-03-05 15:26:21 -0700

    asdf

--- scripts/cibw_before_all_windows.sh
+++ scripts/cibw_before_all_windows.sh
@@ -9,18 +9,19 @@ set -e -x
 # choco upgrade -y --no-progress winflexbison3 swig  # libicu-devel readline-devel
 # export PATH="/c/msys2/usr/bin/:${PATH}"
 # export LDFLAGS="-L/c/msys2/usr/lib"
-pacman -S --noconfirm --needed  \
-          base-devel  \
-          bison  \
-          flex  \
-          mingw-w64-i686-toolchain  \
-          mingw-w64-x86_64-toolchain  \
-          # mingw-w64-cross-toolchain  \
-          mingw-w64-x86_64-libtool  \
-          mingw-w64-x86_64-icu  \
-          icu-devel  \
-          glib2-devel  \
-          swig
+
+# pacman -S --noconfirm --needed  \
+#           base-devel  \
+#           bison  \
+#           flex  \
+#           mingw-w64-i686-toolchain  \
+#           mingw-w64-x86_64-toolchain  \
+#           # mingw-w64-cross-toolchain  \
+#           mingw-w64-x86_64-libtool  \
+#           mingw-w64-x86_64-icu  \
+#           icu-devel  \
+#           glib2-devel  \
+#           swig
 
 cd hfst_src/
 autoreconf -fvi
