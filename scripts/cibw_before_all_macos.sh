25124cba1b39500810eaf0942853df8fa06dc305
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-03-13 19:38:28 -0700

    fix macos stuff

--- scripts/cibw_before_all_macos.sh
+++ scripts/cibw_before_all_macos.sh
@@ -5,7 +5,7 @@
 
 set -e -x
 
-brew install automake bison flex icu4c  # to build hfst from source
+brew install automake bison flex  # to build hfst from source
 export PATH="/usr/local/opt/bison/bin:/usr/local/opt/flex/bin:$PATH"
 export CPPFLAGS="-I/usr/local/opt/flex/include -I/usr/local/opt/icu4c/include"
 export LDFLAGS="-L/usr/local/opt/bison/lib -L/usr/local/opt/flex/lib -L/usr/local/opt/icu4c/lib"
@@ -23,33 +23,28 @@ brew install swig
 # cp tmp/hfst/lib/libhfst.dylib hfst/lib/
 # export LDFLAGS="-L$(pwd)/tmp/hfst/lib ${LDFLAGS}"
 
-# export MACOSX_DEPLOYMENT_TARGET=10.9
-echo "ENV DEBUG"
-env
-
 # Build ICU (brew has too recent MACOSX_DEPLOYMENT_TARGET)
-# mkdir icu_tmp
-# cd icu_tmp/
-# wget https://github.com/unicode-org/icu/archive/release-68-2.tar.gz
-# tar -xzf release-68-2.tar.gz
-# cd icu-release-68-2/icu4c/source/
-# ./runConfigureICU MacOSX/GCC
-# make
-# make install
-# cd ../../../../
+brew uninstall icu4c
+mkdir icu_tmp
+cd icu_tmp/
+wget https://github.com/unicode-org/icu/archive/release-68-2.tar.gz
+tar -xzf release-68-2.tar.gz
+cd icu-release-68-2/icu4c/source/
+./runConfigureICU MacOSX/GCC
+make
+make install
+cd ../../../../
 
 # Build HFST from source
 echo "deployment target: ${MACOSX_DEPLOYMENT_TARGET}"
 cd libhfst_src/
 autoreconf -fvi
 ./configure --with-readline  \
-            --with-unicode-handler=icu  \
-            MACOSX_DEPLOYMENT_TARGET=10.9
+            --with-unicode-handler=icu
 make
 make check V=1 VERBOSE=1
 make install
 cd ..
 
-python -VV
-python3 -VV
-python setup.py build_ext
+python3 setup.py build_ext
+python3 setup.py sdist
