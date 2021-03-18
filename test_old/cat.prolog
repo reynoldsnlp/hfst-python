28dcd8e2c4125438579be75fc3f9d75d321789ef
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-03-18 17:41:19 -0600

    rm test_old/

--- test_old/cat.prolog
+++
@@ -1,5 +0,0 @@
-network(CAT).
-arc(CAT, 0, 1, "c").
-arc(CAT, 1, 2, "a").
-arc(CAT, 2, 3, "t").
-final(CAT, 3).
--- test_old/test4.py
+++
@@ -1,7 +0,0 @@
-import libhfst
-istr = libhfst.HfstInputStream()
-while not istr.is_eof():
-    tr = istr.read()
-    print('Read transducer:')
-    print(tr)
-istr.close()
