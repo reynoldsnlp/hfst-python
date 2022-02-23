6cf69b5c210c920b8c9d9c475fe08687298e262b
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2022-02-23 09:34:09 -0700

    move cibw script to pyproject.toml

--- scripts/cibw_before_all_linux.sh
+++ scripts/cibw_before_all_linux.sh
@@ -8,34 +8,8 @@ set -e -x
 # yum update -d 1
 yum install -y bison flex libicu-devel readline-devel
 
-# manually install swig 4 since yum has 3 :(
-# yum install -y pcre-devel
-# curl https://managedway.dl.sourceforge.net/project/swig/swig/swig-4.0.2/swig-4.0.2.tar.gz > /tmp/swig-4.0.2.tar.gz
-# pushd /tmp
-# tar -zxf swig-4.0.2.tar.gz
-# cd swig-4.0.2
-# ./configure --disable-perl --disable-ruby --disable-csharp --disable-r --disable-java
-# make
-# make install
-# popd
-
-
-# pypy 2010 manylinux image has outdated bison
-BISON_VERSION=$(bison --version | python -c "import re, sys; print(re.search(r'([0-9]+)\.[0-9]+\.[0-9]+.*', sys.stdin.read(), re.S).group(1))")
-echo ${BISON_VERSION}
-if [[ ${BISON_VERSION} < 3 ]]; then
-    echo "Bison is too old. Upgrading...";
-    curl http://mirrors.ibiblio.org/gnu/ftp/gnu/bison/bison-3.7.5.tar.gz > /tmp/bison-3.7.5.tar.gz
-    pushd /tmp
-    tar -zxf bison-3.7.5.tar.gz
-    cd bison-3.7.5
-    ./configure
-    make
-    make install
-    popd
-else
-    echo "Bison is new enough. No upgrade needed.";
-fi
+# bash scripts/install_swig_from_src.sh
+bash scripts/install_bison_from_src.sh
 
 cd libhfst_src/
 autoreconf -fvi
