62e7e2f99546763567c73857cb4adf504318d723
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2023-02-02 13:38:14 -0700

    brew install foma openfst

--- scripts/cibw_before_all_macos.sh
+++ scripts/cibw_before_all_macos.sh
@@ -24,25 +24,30 @@ export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
 # cp tmp/hfst/lib/libhfst.dylib hfst/lib/
 # export LDFLAGS="-L$(pwd)/tmp/hfst/lib ${LDFLAGS}"
 
+brew install icu4c
 # Build ICU (brew has too recent MACOSX_DEPLOYMENT_TARGET)
-brew uninstall --ignore-dependencies icu4c
-mkdir icu_tmp
-cd icu_tmp/
-wget -nv https://github.com/unicode-org/icu/archive/release-68-2.tar.gz
-tar -xzf release-68-2.tar.gz
-cd icu-release-68-2/icu4c/source/
-./runConfigureICU MacOSX/GCC
-make
-make install
-cd ../../../../
+# brew uninstall --ignore-dependencies icu4c
+# mkdir icu_tmp
+# cd icu_tmp/
+# wget -nv https://github.com/unicode-org/icu/archive/release-68-2.tar.gz
+# tar -xzf release-68-2.tar.gz
+# cd icu-release-68-2/icu4c/source/
+# ./runConfigureICU MacOSX/GCC
+# make
+# make install
+# cd ../../../../
 # cp /usr/local/lib/libicu* .  # TODO For some reason `delocate` looks here for dylibs
+
+# install back ends
+brew install foma openfst
 
 # Build HFST from source
 echo "deployment target: ${MACOSX_DEPLOYMENT_TARGET}"
 cd libhfst_src/
 autoreconf -fvi
-./configure --with-unicode-handler=icu
-make -C back-ends
+# ./configure --with-unicode-handler=icu
+./configure --disable-static --with-readline --with-unicode-handler=icu --with-openfst-upstream --with-foma-upstream --enable-python-bindings
+# make -C back-ends
 make -C libhfst
 cd ..
 
