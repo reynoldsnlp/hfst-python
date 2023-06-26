b5937e133708222ee3a35cb5259f1bee5c0c91ee
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2023-06-25 21:04:50 -0600

    fix tar arguments

--- scripts/get_latest_hfst.sh
+++ scripts/get_latest_hfst.sh
@@ -2,10 +2,12 @@ set -e -x
 
 curl https://apertium.projectjj.com/osx/nightly/x86_64/hfst-latest.x86_64.tar.bz2 -o hfst-latest.x86_64.tar.bz2
 curl https://apertium.projectjj.com/osx/nightly/arm64/hfst-latest.arm64.tar.bz2  -o hfst-latest.arm64.tar.bz2
-tar --one-top-level -xf hfst-latest.x86_64.tar.bz2
-tar --one-top-level -xf hfst-latest.arm64.tar.bz2
+tar -xf hfst-latest.x86_64.tar.bz2 -C hfst-x86_64
+tar -xf hfst-latest.arm64.tar.bz2 -C hfst-arm64
 
 uname -m
 python -c "import platform; print('version:', platform.version()); print('uname:', platform.uname())"
+
+. scripts/run_install_nightly_macos.sh
 
 # Set environment variables
