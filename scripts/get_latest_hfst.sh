062b0c2c0b505e81e41bc4a17f8c2fd34128744e
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2023-06-25 21:08:16 -0600

    mkdir

--- scripts/get_latest_hfst.sh
+++ scripts/get_latest_hfst.sh
@@ -1,8 +1,11 @@
 set -e -x
 
 curl https://apertium.projectjj.com/osx/nightly/x86_64/hfst-latest.x86_64.tar.bz2 -o hfst-latest.x86_64.tar.bz2
-curl https://apertium.projectjj.com/osx/nightly/arm64/hfst-latest.arm64.tar.bz2  -o hfst-latest.arm64.tar.bz2
+mkdir hfst-x86_64
 tar -xf hfst-latest.x86_64.tar.bz2 -C hfst-x86_64
+
+curl https://apertium.projectjj.com/osx/nightly/arm64/hfst-latest.arm64.tar.bz2  -o hfst-latest.arm64.tar.bz2
+mkdir hfst-arm64
 tar -xf hfst-latest.arm64.tar.bz2 -C hfst-arm64
 
 uname -m
