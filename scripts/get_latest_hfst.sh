3da246fa38ced5f51cbe5f19b7e6800660c87b07
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2023-06-26 21:20:20 -0600

    install icu

--- scripts/get_latest_hfst.sh
+++ scripts/get_latest_hfst.sh
@@ -1,5 +1,25 @@
 set -e -x
 
+brew install \
+    icu4c
+#     autoconf \
+#     automake \
+#     cmake \
+#     gawk \
+#     gcc \
+#     gperftools \
+#     help2man \
+#     pcre \
+#     perl518 \
+#     pkg-config \
+#     wget
+
+ln -s /usr/local/Cellar/icu4c/73.2/bin/icu-config /usr/local/bin/icu-config
+ln -s /usr/local/Cellar/icu4c/73.2/include/* /usr/local/include
+# export CPPFLAGS="-I/usr/local/include -I/usr/local//Cellar/icu4c/73.2/include" # -I/usr/local/include/unicode -I/usr/local/opt/icu4c/include"
+# export LDFLAGS="-L/usr/local/lib"  # -L/usr/local/opt/bison/lib -L/usr/local/opt/flex/lib -L/usr/local/opt/icu4c/lib"
+# export PKG_CONFIG_PATH="/usr/local/opt/icu4c/lib/pkgconfig"
+
 curl https://apertium.projectjj.com/osx/nightly/x86_64/hfst-latest.x86_64.tar.bz2 -o hfst-latest.x86_64.tar.bz2
 mkdir hfst-x86_64
 tar -xf hfst-latest.x86_64.tar.bz2 -C hfst-x86_64
