7eb7d8e5ca3c0269e0bdb47c8d7b5a4f742f9739
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2023-06-26 23:52:22 -0600

    arch libs

--- scripts/get_latest_hfst.sh
+++ scripts/get_latest_hfst.sh
@@ -1,32 +1,19 @@
 set -e -x
 
-brew install \
-    icu4c
-#     autoconf \
-#     automake \
-#     cmake \
-#     gawk \
-#     gcc \
-#     gperftools \
-#     help2man \
-#     pcre \
-#     perl518 \
-#     pkg-config \
-#     wget
+brew install icu4c
+brew install icu4c --force --bottle-tag=arm64_big_sur
 
 ln -s /usr/local/Cellar/icu4c/73.2/bin/icu-config /usr/local/bin/icu-config
 ln -s /usr/local/Cellar/icu4c/73.2/include/* /usr/local/include
-# export CPPFLAGS="-I/usr/local/include -I/usr/local//Cellar/icu4c/73.2/include" # -I/usr/local/include/unicode -I/usr/local/opt/icu4c/include"
-# export LDFLAGS="-L/usr/local/lib"  # -L/usr/local/opt/bison/lib -L/usr/local/opt/flex/lib -L/usr/local/opt/icu4c/lib"
-# export PKG_CONFIG_PATH="/usr/local/opt/icu4c/lib/pkgconfig"
 
 curl https://apertium.projectjj.com/osx/nightly/x86_64/hfst-latest.x86_64.tar.bz2 -o hfst-latest.x86_64.tar.bz2
 curl https://apertium.projectjj.com/osx/nightly/x86_64/foma-latest.x86_64.tar.bz2 -o foma-latest.x86_64.tar.bz2
 mkdir hfst-x86_64
 tar -xf hfst-latest.x86_64.tar.bz2 -C hfst-x86_64
 tar -xf foma-latest.x86_64.tar.bz2 -C hfst-x86_64
-cp -RP hfst-x86_64/hfst/lib/*.dylib /usr/local/lib/
-cp -RP hfst-x86_64/foma/lib/*.dylib /usr/local/lib/
+file hfst-x86_64/hfst/lib/* hfst-x86_64/foma/lib/*
+# cp -RP hfst-x86_64/hfst/lib/*.dylib /usr/local/lib/
+# cp -RP hfst-x86_64/foma/lib/*.dylib /usr/local/lib/
 
 # curl https://apertium.projectjj.com/osx/nightly/arm64/hfst-latest.arm64.tar.bz2  -o hfst-latest.arm64.tar.bz2
 # mkdir hfst-arm64
