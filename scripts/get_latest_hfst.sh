e1f7cf9df81daf2190a2f48bdaab5404958db600
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2023-06-27 02:29:14 -0600

    no foma.pc

--- scripts/get_latest_hfst.sh
+++ scripts/get_latest_hfst.sh
@@ -8,7 +8,6 @@ mkdir hfst-x86_64
 tar -xf hfst-latest.x86_64.tar.bz2 -C hfst-x86_64
 tar -xf foma-latest.x86_64.tar.bz2 -C hfst-x86_64
 cp hfst-x86_64/hfst/lib/pkgconfig/hfst.pc /usr/local/lib/pkgconfig/
-cp hfst-x86_64/foma/lib/pkgconfig/foma.pc /usr/local/lib/pkgconfig/
 
 
 curl https://apertium.projectjj.com/osx/nightly/arm64/hfst-latest.arm64.tar.bz2  -o hfst-latest.arm64.tar.bz2
